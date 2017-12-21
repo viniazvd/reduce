65534376c369f29536491cf8a1245daa38bfbf57
Vinicius Azevedo <viniazvd@gmail.com>
2017-12-21 13:48:00 -0200

    refactoring with spread operator

--- src/prototypes/filter.js
+++ src/prototypes/filter.js
@@ -1,6 +1,6 @@
 const filter = (array, fn) => {
   return array.reduce((acc, cur) => {
-    if (fn(cur)) acc.push(cur)
+    if (fn(cur)) acc = [...acc, cur] // acc.push(cur)
 
     return acc
   }, [])
